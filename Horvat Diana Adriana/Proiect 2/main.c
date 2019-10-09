ebd479a36ba39cdc18cb36289cf348d1061dce3a
dianaahorvat <horvat.diana2000@gmail.com>
2019-10-09 11:36:41 +0300

    Problema 3.5

---
+++ Horvat Diana Adriana/Proiect 2/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x;
+    scanf("%f",&x);
+    if(x<-2)
+    {
+        printf("%f",x*x-7*x+4);
+    }
+    if(x==-2)
+    {
+        printf("%f",0);
+    }
+    if(x>-2)
+    {
+        printf("%f",x*x+5*x-2);
+    }
+    return 0;
+}
