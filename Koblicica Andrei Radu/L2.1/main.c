0c75fc9e1db4484173367c7ed72e4da0f4fdaf5b
student <student@obs.cs.utcluj.ro>
2019-10-09 11:40:09 +0300

    L2

---
+++ Koblicica Andrei Radu/L2.1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("size of char:%d bytes\n",sizeof(char));
+    printf("size of short:%d bytes\n",sizeof(short));
+    printf("size of int:%d bytes\n",sizeof(int));
+    printf("size of long:%d bytes\n",sizeof(long));
+    printf("size of long long:%d bytes\n",sizeof(long long));
+    printf("size of float:%d bytes\n",sizeof(float));
+    printf("size of double:%d bytes\n",sizeof(double));
+    printf("size of long double:%d bytes\n",sizeof(long double));
+    return 0;
+}
