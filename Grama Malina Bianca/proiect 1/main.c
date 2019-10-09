5b38df6ddcfb39d97c08e7cae2a73f6c8f5961c9
Grama Malina <mxligr@Gramas-MacBook-Pro.local>
2019-10-09 11:41:15 +0300

    pb 3.5

---
+++ Grama Malina Bianca/proiect 1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of char :%d byte(s) \n" , sizeof(char) );
+    printf("Size of short :%d byte(s) \n", sizeof(short));
+    printf("Size of int :%d byte(s) \n", sizeof(int));
+    printf("Size of long :%d byte(s) \n", sizeof(long));
+    printf("Size of long double :%d byte(s) \n", sizeof(long double));
+    printf("Size of float :%d byte(s) \n", sizeof(float));
+
+    return 0;
+}
