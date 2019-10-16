8a4b11002522e80a3a8b85e26359e64bd08f34c6
Groza Dominic <groza.dominik@yahoo.de>
2019-10-16 11:12:04 +0300

    Exercise 3.3 (min max)

--- Groza Dominic/First/main.c
+++ Groza Dominic/First/main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-        float age;
-        float heightcm;
-        char gender [10];
-    double W_male = 50+0.75*(heightcm-150)+(age-20)/4;
-    double W_fema = W_male -10;
-    printf ("Enter your gender (male or female) ,your height (centimeters) and your age\n");
-    scanf ("%s,%f,%f",&gender,&heightcm,&age);
-    if (strcmp(gender,"male")==0){
-        printf("%d",W_male);}
-        else{
-           printf("%d",W_fema);
+int main() {
+    char gender[10];
+    double W_male, W_female, age, heightcm;
+    printf("Enter your gender (male or female) ,your height (centimeters) and your age\n");
+    scanf("%s", &gender);
+    scanf("%Lf", &heightcm);
+    scanf("%Lf", &age);
+    W_male = 50 + 0.75 * (heightcm - 150) + (age - 20) / 4;
+    W_female = W_male - 10;
+    if (strcmp(gender, "male") == 0) {
+        printf("%Lf", W_male);
+    } else {
+        printf("%Lf", W_female);
     }
     return 0;
 
