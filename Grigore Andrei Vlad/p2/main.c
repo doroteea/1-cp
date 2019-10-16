28b5bc5974a83acae174b4607154c5112bb703bc
Andrei <grigoreandreivlad@gmail.com>
2019-10-16 10:20:17 +0300

    p2

---
+++ Grigore Andrei Vlad/p2/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{float x,s;
+scanf("%f",&x);
+if(x<-2)
+{
+    s=x*x-7*x+4;
+}
+    else if(x==-2)
+    {
+        s=0;
+    }
+    else {s=x*x+5*x-2;}
+    printf("%f",s);
+
+    return 0;
+}
