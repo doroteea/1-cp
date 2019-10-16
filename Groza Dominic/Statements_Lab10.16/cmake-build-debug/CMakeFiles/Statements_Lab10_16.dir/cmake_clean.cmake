9dad2d165c5183d659fd73e08099c0f76f148bfe
Groza Dominic <groza.dominik@yahoo.de>
2019-10-16 11:09:59 +0300

    Exercise 3.3 (min max)

---
+++ Groza Dominic/Statements_Lab10.16/cmake-build-debug/CMakeFiles/Statements_Lab10_16.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Statements_Lab10_16.dir/main.c.obj"
+  "Statements_Lab10_16.exe"
+  "Statements_Lab10_16.exe.manifest"
+  "Statements_Lab10_16.pdb"
+  "libStatements_Lab10_16.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Statements_Lab10_16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
