a4f2233c62ff6796155213d42824dd98df8d3d93
adrianapopa26 <popaadriana2606@gmail.com>
2019-10-19 19:02:34 +0300

    sdf

---
+++ Popa Adriana/lab2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Size of char %d bytes",sizeof(char));
+    printf("\nSize of short %d bytes",sizeof(short));
+    printf("\nSize of long %d bytes",sizeof(long));
+    printf("\nSize of int %d bytes",sizeof(int));
+    printf("\nSize of longint %d bytes",sizeof(long int));
+    return 0;
+}
