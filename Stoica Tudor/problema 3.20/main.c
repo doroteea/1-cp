6ef39cce046d8d3810c6bfc1e982af89145e75df
Citadin2 <56082968+Citadin2@users.noreply.github.com>
2019-10-22 00:10:20 +0300

    Update main.c

--- Stoica Tudor/problema 3.20/main.c
+++ Stoica Tudor/problema 3.20/main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 char prop[300],*token;
 const char delim[20]=" .?;:,!";
-
+int cnt;
 
 int main() {
 
@@ -11,8 +11,10 @@ int main() {
 
     while (token != NULL) {
         printf("%s\n",token);
+        cnt++;
         token = strtok(NULL, delim);
     }
+    printf("%d", cnt);
 /// for Ana are mere it only couts Ana
 /// for any other Ana.are.mere it couts Ana are mere
     return 0;
