63536f8e9b38db88349c902461b5299500a5f151
Andrei <grigoreandreivlad@gmail.com>
2019-10-22 19:09:48 +0300

    brown fox

--- Grigore Andrei Vlad/statements/main.c
+++ Grigore Andrei Vlad/statements/main.c
@@ -2,29 +2,27 @@
 #include <stdlib.h>
 
 int main()
-{int i,length,min,max,a[length];
-printf("Enter length\n");
-scanf("%d",&length);
-printf("enter numbers\n");
-for(i=0;i<length;i++)
-{scanf("%d",&a[i]);
+{unsigned n,i,d,OK;
+scanf("%d",&n);
+printf("The prime numbers are\n");
+for(i=1;i<=n;i++)
+{OK=1;
+    for(d=2;d<=i/2;d++)
+    {
+        if(i%d==0)
+        {
+            OK=0;
+            break;
+        }
+
+        if(OK=1)
+        {
+            printf("&d",i);
+        }
+
+    }
 
 }
-max=a[0];
-min=a[0];
-for(i=1;i<length;i++)
-{
-    if(a[i]>max)
-    {
-        max=a[i];
-    }
-    if(a[i]<min)
-    {
-        min=a[i];
-    }
-}
-printf("Maximum element=%d\n",max);
-printf("Minimum element=%d\n",min);
 
     return 0;
 }
