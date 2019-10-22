36d6328f4d72da1652751247fde7212eba819162
Andrei <grigoreandreivlad@gmail.com>
2019-10-22 20:57:36 +0300

    senna

--- Grigore Andrei Vlad/Senna/main.c
+++ Grigore Andrei Vlad/Senna/main.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 
 int main()
-{int n,i,aux,x;
-scanf("%d %d",&n,&x);
-for(i=1;i<=n;i++)
 {
-    scanf("%d",&aux);
-    if(aux==x)
+    int n,i,aux,x;
+    scanf("%d %d",&n,&x);
+    for(i=0; i<=n; i++)
     {
+        scanf("%d",&aux);
+        if(aux==x)
+        {
             printf("The number and position are:%d %d",x,i);
+        }
+
+
     }
-
-
-}
     return 0;
 }
