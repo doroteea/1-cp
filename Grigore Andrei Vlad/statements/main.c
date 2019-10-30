7f017f5d5405acb2ac07dde18861ce685aaab229
Andrei <grigoreandreivlad@gmail.com>
2019-10-30 10:04:31 +0200

    state

--- Grigore Andrei Vlad/statements/main.c
+++ Grigore Andrei Vlad/statements/main.c
@@ -2,27 +2,29 @@
 #include <stdlib.h>
 
 int main()
-{unsigned n,i,d,OK;
-scanf("%d",&n);
-printf("The prime numbers are\n");
-for(i=1;i<=n;i++)
-{OK=1;
-    for(d=2;d<=i/2;d++)
+{
+    unsigned n,i,d,OK;
+    scanf("%d",&n);
+    printf("The prime numbers are\n");
+    for(i=1; i<=n; i++)
     {
-        if(i%d==0)
+        OK=1;
+        for(d=2; d<=i/2; d++)
         {
-            OK=0;
-            break;
-        }
+            if(i%d==0)
+            {
+                OK=0;
+                break;
+            }
 
-        if(OK=1)
-        {
-            printf("&d",i);
+            if(OK==1)
+            {
+                printf("Positions are:%d",i);
+            }
+
         }
 
     }
-
-}
 
     return 0;
 }
