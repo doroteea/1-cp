06239976d17b09cf81e75ac119adda2d16b35b61
DariusBotusanu <botusanu.darius31@outlook.com>
2019-11-03 19:33:57 +0200

    4.9 still bugs me, what is "expected value before int" even supposed to mean? And why too few arguments?

--- Botusanu Darius-Gabriel/lab 4, 3.9 THIS ONE PLEASE/main.c
+++ Botusanu Darius-Gabriel/lab 4, 3.9 THIS ONE PLEASE/main.c
@@ -2,76 +2,90 @@
 #include <stdlib.h>
 #define SIZE 200
 
-/*I can not understand the "expected ) bevore [" on line 27 or 28*/
-int max(a,b)
-{
-    int c;
-    if(a>=b){
-        c=a;
-    }
-    else{
-        c=b;
-    }
-    return c;
-}
-int min(a,b)
-{
-    int c;
-    if(a>=b){
-        c=b;
-    }
-    else{
-        c=a;
-    }
-    return c;
-}
+int min(int *a, int *b);
 
-void addition(degA, degB, coeffA[SIZE], coeffB[SIZE])
-{
-    int i, aux;
-    float sum[SIZE];
-    for(i=0;i<min(degA,degB);i++)
-    {
-        sum[i]=coeffA[i]+coeffB[i];
-    }
-    for(i=min(degA,degB);i<=max(degA,degB);i++)
-    {
-        if(degA>=degB)
-        {
-            sum[i]=coeffA[i];
-        }
-        else
-        {
-            sum[i]=coeffB[i];
-        }
-    }
-    for(i=0;i<=max(degA,degB);i++)
-    {
-        printf("\nsum[%d]=%f",i,sum[i]);
-    }
+int max(int *a, int *b);
 
-}
+void addition(int degA, float coeffA[degA], int degB, float coeffB[degB]);
 
 int main()
 {
-    int degA, degB, i, j;
-    printf("Input the degree of polynomial A: ");
+    int degA, degB, i;
+    printf("Please input the degree of polynomial A:");
     scanf("%d", &degA);
-    float coeffA[degA];
-    float coeffB[degB];
-    for(i=0;i<=degA;i++)
+
+    printf("\nPlease input the degree of polynomial B:");
+    scanf("%d", &degB);
+
+    float coeffA[degA], coeffB[degB];
+
+    for(i=0; i<=degA; i++)
     {
         printf("\ncoeffA[%d]=",i);
-        scanf("%f",&coeffA[i]);
+        scanf("%f", &coeffA[i]);
     }
-    printf("\nInput the degree of polynomial B: ");
-    scanf("%d", &degB);
-    for(j=0;j<=degB;j++)
+
+    for(i=0; i<=degB; i++)
     {
-        printf("\ncoeffB[%d]=",j);
-        scanf("%f",&coeffB[j]);
+        printf("\ncoeffB[%d]=",i);
+        scanf("%f", &coeffB[i]);
     }
-    addition(degA, degB, coeffA[SIZE], coeffB[SIZE]);
+    addition(int degA, float coeffA[degA], int degB, float coeffB[degB]);
+
     return 0;
 
+}
+
+void addition(int degA, float coeffA[degA], int degB, float coeffB[degB])
+{
+    int i;
+    float coeffC[SIZE];
+
+    for(i=0; i<min(&degA, &degB); i++)
+    {
+        coeffC[i]=coeffA[i]+coeffB[i];
+    }
+    for(i=min(&degA, &degB); i<=max(&degA, &degB); i++)
+    {
+        if(degA>degB)
+        {
+            coeffC[i]=coeffA[i];
+        }
+        else
+        {
+            coeffC[i]=coeffB[i];
+        }
+    }
+    for(i=0; i<=max(&degA, &degB); i++)
+    {
+        printf("\ncoeffC[%d]=%f; ", i, coeffC[i]);
+    }
+}
+
+int min(int *a, int *b)
+{
+    int aux;
+    if(*a<=*b)
+    {
+        aux=*a;
+    }
+    else
+    {
+        aux=*b;
+    }
+    return aux;
+}
+
+int max(int *a, int *b)
+{
+    int aux;
+    if(*a<=*b)
+    {
+        aux=*b;
+    }
+    else
+    {
+        aux=*a;
+    }
+    return aux;
 }
