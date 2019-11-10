ebf6916a03cc1006fb7b20a18f365e8aa088888a
andreimuth <56334618+andreimuth@users.noreply.github.com>
2019-11-10 12:00:47 +0200

    Add files via upload
    I succeeded to solve 3.8. At 3.6 I tried to "divide" a float number in two integers (for example 4.53 -> 4 and 53), but I encounter errors at the fractional part...

--- 3.8.c
+++ 3.8.c
@@ -1,32 +1,87 @@
-#include <stdlib.h>
-
 #include <stdio.h>
-
+#include <stdlib.h>
 #include <string.h>
 
-int main() {
-    struct book {
-        char author[26];
-        char title[36];
-        int year;
-    }
-    aux, list[55];
-    int n;
-    printf("n = ");
-    scanf("%d", & n);
+struct book
+{
+    char author[26];
+    char title[26];
+    int year;
+};
+
+void alphabeticOrderByAuthor (struct book list[55], int n)
+{
+    struct book aux;
     int i, j;
-    for (i = 0; i < n; i++) {
-        fgets(list[i].author, 25, stdin);
-        getchar();
-        fgets(list[i].title, 35, stdin);
-        getchar();
-        scanf("%d", & list[i].year);
-        fflush(stdin);
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (strcmp (list[i].author, list[j].author) > 0)
+            {
+                aux = list[i];
+                list[i] = list[j];
+                list[j] = aux;
+            }
+        }
+    }
+}
+
+void orderByYear (struct book list[55], int n)
+{
+    int i, j;
+    struct book aux;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (list[i].year > list[j].year)
+            {
+                aux = list[i];
+                list[i] = list[j];
+                list[j] = aux;
+            }
+        }
     }
 
-    printf("the authors in alphabetic order are:\n");
-    for (i = 0; i < n; i++) {
-        printf("%s\n", list[i].author);
+}
+
+int main ()
+{
+    struct book list[55];
+    int n, i;
+    printf ("n = ");
+    scanf ("%d", &n);
+    getchar ();
+    for (i = 0; i < n; i++)
+    {
+        printf ("author book#%d: ", i);
+        scanf ("%[^\n]", list[i].author);
+        getchar ();
+        printf ("title book#%d: ", i);
+        scanf ("%[^\n]", list[i].title);
+        getchar ();
+        printf ("year of publication of book#%d: ", i);
+        scanf ("%d", &list[i].year);
+        getchar ();
     }
+
+    alphabeticOrderByAuthor (list, n);
+
+    printf ("*********************\nThe authors in alphabetic order are:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf ("%s\n", list[i].author);
+    }
+
+    orderByYear (list, n);
+
+    printf ("*********************\nThe order of the publication year is:\n");
+
+    for (i = 0; i < n; i++)
+    {
+        printf ("%s - %s (%d)\n", list[i].author, list[i].title, list[i].year);
+    }
+
     return 0;
 }
