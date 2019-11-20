b3b6b78eb223706f0723cced1f5db62845ced63f
andreimuth <56334618+andreimuth@users.noreply.github.com>
2019-11-20 09:30:48 +0200

    Add files via upload
    3.3

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "student.h"
+
+int main()
+{
+    student *group = (student*) malloc (sizeof (student) * 100);
+    int n;
+    printf ("How many students? n = ");
+    scanf ("%d", &n);
+    getchar ();
+    for (int i = 0; i < n; i++)
+    {
+        group[i] = readStud (group, i);
+    }
+    group = sortStudents (group, n);
+    printStudents (group, n);
+    return 0;
+}
+
---
+++ student.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "student.h"
+
+student readStud (student *group, int i)
+{
+    printf ("name of student#%d: ", i);
+    group[i].name = (char*) malloc (sizeof (char) * 35);
+    gets (group[i].name);
+    printf ("date of birth (day, month year): ");
+    scanf ("%d", &group[i].birthDate.day);
+    scanf ("%d", &group[i].birthDate.month);
+    scanf ("%d", &group[i].birthDate.year);
+    getchar ();
+    printf ("residence: ");
+    group[i].residence = (char*) malloc (sizeof (char) * 100);
+    gets (group[i].residence);
+    return group[i];
+}
+
+void printStudents (student *group, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf ("%s (%d.%d.%d) - %s\n", group[i].name, group[i].birthDate.day, group[i].birthDate.month, group[i].birthDate.year, group[i].residence);
+    }
+}
+
+student * sortStudents (student *group, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (strcmp (group[i].name, group[j].name) > 0)
+            {
+                student aux;
+                aux = group[i];
+                group[i] = group[j];
+                group[j] = aux;
+            }
+        }
+    }
+    return group;
+}
