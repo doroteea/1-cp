3b503dd23291078f7d1f2e9106dad53b63045d42
adrianapopa26 <popaadriana2606@gmail.com>
2019-11-26 20:47:35 +0200

    lab 10 problemele 6,9

---
+++ Popa Adriana/lab10pb6/main.c
@@ -0,0 +1,9 @@
+#include "union.h"
+//problema asta nu merge dar nu imi dau seama de ce
+int main()
+{
+    Union U=readElements();
+    Areas A=calculateAreas(U);
+    printAreas(A);
+    return 0;
+}
