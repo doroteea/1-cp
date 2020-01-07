72b022baca001de17e96a0e2858b3036d7cd3d15
Groza Dominic <groza.dominik@yahoo.de>
2020-01-08 00:57:32 +0200

    Added Explorers Problem, sorry, I use Internet Explorer

---
+++ Groza Dominic/HolyGrail/cmake-build-debug/CMakeFiles/HolyGrail.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HolyGrail.dir/main.c.obj"
+  "HolyGrail.exe"
+  "HolyGrail.exe.manifest"
+  "HolyGrail.pdb"
+  "libHolyGrail.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HolyGrail.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
